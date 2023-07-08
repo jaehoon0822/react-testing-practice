e84761c157436c4280752e16ceb36686a8ec76ab
jhoon <jaehoon0822@gmail.com>
2023-07-08 10:57:08 +0900

    unit test 개념 확인

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 test("button has correct initial button", () => {
   render(<App />);
@@ -45,7 +45,7 @@ test("checkbox 첫번째 클릭시 button disabled, 두번째 클릭시 button e
   const colorButton = screen.getByRole("button", { name: "change to blue" });
 
   const checkbox = screen.getByRole("checkbox", {
-    name: "desable-btn",
+    name: "disable button",
   });
 
   fireEvent.click(checkbox);
@@ -53,4 +53,32 @@ test("checkbox 첫번째 클릭시 button disabled, 두번째 클릭시 button e
 
   fireEvent.click(checkbox);
   expect(colorButton).toBeEnabled();
+});
+
+test("checkbox 클릭시 색상 gray 로 변경", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", { name: "change to blue" });
+
+  const checkbox = screen.getByRole("checkbox", { name: "disable button" });
+
+  fireEvent.click(checkbox);
+
+  expect(colorButton).toHaveStyle({
+    "background-color": "gray",
+  });
+});
+
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+
+  test("Works for one inner capital letter", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("Works for mulitple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MidiumVioletRed")).toBe("Midium Violet Red");
+  });
 });
--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useMemo, useState } from "react";
 import "./App.css";
 
+export function replaceCamelWithSpaces(colorName: string) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
+
 function App() {
-  const [isDesabled, setIsDesabled] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const [buttonColor, setButtonColor] = useState<String>("blue");
-  const buttonStyle: React.CSSProperties = useMemo(
-    () => ({
-      backgroundColor: buttonColor === "red" ? "blue" : "red",
+
+  const buttonStyle: React.CSSProperties = useMemo(() => {
+    const bgColor = buttonColor === "red" ? "blue" : "red";
+
+    return {
+      backgroundColor: isDisabled ? "gray" : bgColor,
       color: "white",
-    }),
-    [buttonColor]
-  );
+    };
+  }, [buttonColor, isDisabled]);
 
   const onClickCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsDesabled((_prev) => e.target.checked);
+    setIsDisabled((_prev) => e.target.checked);
   };
 
   const onClickButton = () => {
@@ -22,12 +28,19 @@ function App() {
 
   return (
     <div>
-      <button style={buttonStyle} onClick={onClickButton} disabled={isDesabled}>
+      <button style={buttonStyle} onClick={onClickButton} disabled={isDisabled}>
         {`change to ${buttonColor}`}
       </button>
       <div>
-        <label htmlFor="desable-btn">disable button</label>
-        <input type="checkbox" onChange={onClickCheckbox} id="desable-btn" />
+        <label htmlFor="disable-btn">disable button</label>
+        <input
+          type="checkbox"
+          onChange={onClickCheckbox}
+          defaultChecked={isDisabled}
+          aria-checked={isDisabled}
+          name="disable-btn"
+          id="disable-btn"
+        />
       </div>
     </div>
   );
